089ea834520f2769995592013abb8636f112e511
Abzed <ibraabzed@gmail.com>
2020-09-06 13:21:15 +0300

    add verify,create,save,display and delete credential

--- run.py
+++ run.py
@@ -1,6 +1,29 @@
 #! /usr/bin/env python3
-from user_ import User
+from user_credential import User, Credentials
 
-def create_user(name, password):
-    new_user = User(name, password)
+def create_user(username, password):
+    new_user = User(username, password)
     return new_user
+
+def save_user(user):
+   user.save_user()
+
+def display_user():
+    return User.display_user()
+
+def verify_user(username, password):
+     check_user = Credentials.check_user(username,password)
+     return check_user
+ 
+def create_credentials(account_name,username, password):
+     new_credentials = Credentials(account_name, username, password)
+     return new_credentials
+ 
+def save_credentials(credentials):
+     credentials.save_credentials()
+     
+def display_credentials():
+    return Credentials.display_credentials()
+
+def delete_credentials(credentials):
+    credentials.delete_credentials()
